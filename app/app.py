10f2204ee99bff5d1eee3d8f763d129ac69c5c09
NikoKamtsioras <ckamtsioras@gmail.com>
2022-07-10 13:03:31 -0600

    Upload files

---
+++ app/app.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask import render_template
+
+webapp = Flask(__name__)
+
+
+@webapp.route("/")
+def app():
+    return render_template('home.html')
+
+
+@webapp.route("/blog")
+def blog():
+    return render_template('blog.html')
+
+
+@webapp.route("/about")
+def about():
+    return render_template('about.html')
+
+
+@webapp.route("/contact")
+def contact():
+    return render_template('contact.html')
+
+
+if __name__ == '__main__':
+    webapp.run()
